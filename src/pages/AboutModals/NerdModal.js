de9c7f315ea80ad1f0f524cbb650c5043b8d993d
Teri <50551345+teriaiw@users.noreply.github.com>
2024-03-25 21:46:24 +0800

    Modal add ons

--- src/pages/AboutModals/NerdModal.js
+++ src/pages/AboutModals/NerdModal.js
@@ -1,5 +1,6 @@
 import Modal from 'react-modal';
-import styles from '../about.module.css';
+import styles from './Modal.module.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
 //for Acessibility
 Modal.setAppElement('body');
@@ -11,40 +12,38 @@ function NerdModal({isOpen, onClose}) {
       style={{
         overlay: {
           position: 'fixed',
-          top: '10%',
-          left: '10%',
-          right: '10%',
-          bottom: '10%',
-          backgroundColor: 'rgba(255, 255, 255, 0.75)'
+          backgroundColor: 'rgba(255, 255, 255, 0.55)'
         },
         content: {
           position: 'absolute',
-          top: '40px',
-          left: '40px',
-          right: '40px',
-          bottom: '40px',
+          top: '15%',
+          left: '15%',
+          right: '15%',
+          bottom: '15%',
           border: '1px solid #ccc',
           background: 'pink',
           overflow: 'auto',
           WebkitOverflowScrolling: 'touch',
-          borderRadius: '4px',
+          borderRadius: '25px',
           outline: 'none',
-          padding: '20px'
+          padding: '50px'
         }
       }}
       isOpen={isOpen}
       contentLabel="Example Modal"
+      closeOnOverlayClick={true}
+      onRequestClose={onClose} 
     >
-      <button className={styles.modalCloseButton} onClick={onClose}>close</button>
-      <h2 className={styles.modalHeader}>Hello</h2>
-      <div>I am a modal</div>
-      <form>
-        <input />
-        <button>tab navigation</button>
-        <button>stays</button>
-        <button>inside</button>
-        <button>the modal</button>
-      </form>
+      <button className={styles.modalCloseButton} onClick={onClose}>
+        <FontAwesomeIcon icon="fa-solid fa-xmark" size="2xl" style={{color: "#ffffff",}} />
+      </button>
+      <div className={styles.modalContainer}>
+        <div className={styles.dummyContainer}>Character</div>
+        <h1 className={styles.modalHeader}>Nerd_Teri</h1>
+        <p className={styles.modalContent}>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+        </p>
+      </div>
     </Modal>
     );
 }
